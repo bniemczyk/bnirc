90cb3749ce3fcb1edfd13e9dd76bb17d1de5a6c4
Brandon Niemczyk <brandon@media-center.(none)>
2008-11-30 18:13:12 -0500

    Added script that creates a .bnirc with help from the user if one doens't already exist

---
+++ scripts/mkinitfile.py
@@ -0,0 +1,44 @@
+# script to help a user create a basic .bnirc file
+#
+
+import bnirc
+import os
+
+home_dir = os.getenv("HOME")
+init_file = home_dir + "/.bnirc"
+
+def mkifile():
+	bnirc.ColoredMsg("It appears you do not have a .bnirc file.  We will create one now\n", bnirc.USER_WHITE)
+	nick = bnirc.Ask("What nick name would you like to use?")
+	server = bnirc.Ask("What IRC server would you like to connect to?")
+	channels = bnirc.Ask("What channels would you like to join (seperate with spaces)?").split()
+
+	def fix_channel(c):
+		if c[0] == '#': return c
+		return "#" + c
+
+	channels = [fix_channel(c) for c in channels]
+	seperate = bnirc.Ask("Would you like channels in seperate windows, or in one common window (enter 'common' for 1 window, anything else for seperate)?")
+
+	f = open(init_file, "w")
+	f.write("/set default_nick %s" % (nick))
+	f.write("/server %s" % (server))
+
+	win = 1
+	for i in channels:
+		f.write("/window %d\n" % win)
+		f.write("/join %s\n" % i)
+		if seperate != "common\n": win += 1
+
+	f.close()
+
+	bnirc.ParseInput("source %s" % (init_file))
+	bnirc.ColoredMsg("Don't forget to modify %s later to fit your needs\n" % init_file, bnirc.USER_WHITE)
+	bnirc.ColoredMsg("You can get help at ", bnirc.USER_WHITE)
+	bnirc.ColoredMsg("#bnirc on irc.freenode.net\n", bnirc.USER_BLUE)
+
+try:
+	is_there = open(init_file)
+	is_there.close()
+except:
+	mkifile()
